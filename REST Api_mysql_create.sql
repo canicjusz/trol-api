3418471eda0672a4ab4f6e18b9edcc905db588dc
SealSneezy <jakub.wypychowski@onet.pl>
2021-11-22 13:07:18 +0100

    first commit

---
+++ REST Api_mysql_create.sql
@@ -0,0 +1,37 @@
+CREATE TABLE `Posts` (
+	`ID` int NOT NULL AUTO_INCREMENT,
+	`Date` DATE NOT NULL,
+	`Author` int NOT NULL,
+	`Content` TEXT NOT NULL,
+	`Content-shortened` TEXT NOT NULL,
+	`Categories` TEXT NOT NULL,
+	`Viewcount` int NOT NULL,
+	PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `Categories` (
+	`Name` TEXT NOT NULL,
+	PRIMARY KEY (`Name`)
+);
+
+CREATE TABLE `Authors` (
+	`ID` int NOT NULL AUTO_INCREMENT,
+	`Name` TEXT NOT NULL,
+	`Avatar` blob NOT NULL,
+	`Address` TEXT NOT NULL,
+	`Phone` set NOT NULL,
+	`E-mail` TEXT NOT NULL,
+	`Bio` TEXT NOT NULL,
+	`Posts` json NOT NULL,
+	PRIMARY KEY (`ID`)
+);
+
+ALTER TABLE `Posts` ADD CONSTRAINT `Posts_fk0` FOREIGN KEY (`Author`) REFERENCES `Authors`(`ID`);
+
+ALTER TABLE `Posts` ADD CONSTRAINT `Posts_fk1` FOREIGN KEY (`Categories`) REFERENCES `Categories`(`Name`);
+
+ALTER TABLE `Authors` ADD CONSTRAINT `Authors_fk0` FOREIGN KEY (`Posts`) REFERENCES `Posts`(`ID`);
+
+
+
+
