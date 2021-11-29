6e383cd68a1ca178d9f830f90c03c472ce9b2ed8
canicjusz <janek.bananek371@gmail.com>
2021-11-29 12:08:39 +0100

    #14 jednak wszystko git powinno byc

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 
 class DB {
-         private $host = 'eu-cdbr-west-01.cleardb.com';
-         private $dbname = 'heroku_9a37366182b5a0b';
-         private $user = 'ba84dd51f09433';
-         private $pass = '5de473f9';
+        private $host = 'eu-cdbr-west-01.cleardb.com';
+        private $dbname = 'heroku_9a37366182b5a0b';
+        private $user = 'ba84dd51f09433';
+        private $pass = '5de473f9';
         // private $host = 'localhost';
         // private $user = 'root';
         // private $pass = '';
