afa50653830a3821c0fe8e31840b7309e4649e0b
canicjusz <janek.bananek371@gmail.com>
2021-11-30 10:35:45 +0100

    #18 dodanie CORS wedlug dokumentacji

--- public/index.php
+++ public/index.php
@@ -3,11 +3,24 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Response;
+use Slim\Exception\HttpNotFoundException;
 
 require '../vendor/autoload.php';
 
 $app = AppFactory::create();
 $app->setBasePath("/api");
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+  return $response;
+});
+
+$app->add(function ($request, $handler) {
+  $response = $handler->handle($request);
+  return $response
+          ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+          ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+          ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 
 require '../config/db.php';
 
@@ -15,4 +28,7 @@ require './auth.php';
 require './categories.php';
 require './posts.php';
 
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+  throw new HttpNotFoundException($request);
+});
 $app->run();
