0dea81fdf67f76e8ed11d98abef1e8ee3a7f79e2
Jan Michalak <janek.bananek371@gmail.com>
2021-12-02 10:22:16 +0100

    #22 generowany jest zawsze inny token, wygasa po 10 minutach

--- public/auth.php
+++ public/auth.php
@@ -5,7 +5,8 @@ use Slim\Factory\AppFactory;
 use \Firebase\JWT\JWT;
 
 $secret = "supersecret";
- $app->add(new Tuupola\Middleware\JwtAuthentication([
+
+$app->add(new Tuupola\Middleware\JwtAuthentication([
     "secure" => false,
     "path" => ["/api/posts", "/api/categories"], 
     "algorithm" => ["HS256"],
@@ -16,14 +17,18 @@ $secret = "supersecret";
         return $response
             ->withHeader("Content-Type", "application/json")
             ->getBody()->write(json_encode($data));
-     }
- ]));
+        }
+]));
 
-
- $app->get('/token', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
-     $payload = "XD";
-     global $secret; 
-     $token = JWT::encode($payload, $secret, "HS256");   
-     $response->getBody()->write( json_encode(["status"=>"200","token"=>$token]));
-       return $response;   
- });
+$app->get('/token', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
+    global $secret; 
+    $future = new DateTime("now +10 minutes");
+    $jti = base64_encode(random_bytes(10));
+    $payload = [
+        "jti" => $jti,
+        "exp" => $future->getTimeStamp()
+    ];
+    $token = JWT::encode($payload, $secret, "HS256");   
+    $response->getBody()->write( json_encode(["status"=>"200","token"=>$token]));
+    return $response;   
+});
