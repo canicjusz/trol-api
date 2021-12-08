6062a5edab5bf256382e0fb0e9fa95b5d96574ba
SealSneezy <jakub.wypychowski@onet.pl>
2021-12-08 10:35:54 +0100

    #23 poprawka formuły

--- config/db.php
+++ config/db.php
@@ -1,14 +1,14 @@
 <?php
 
 class DB {
-        // private $host = 'eu-cdbr-west-01.cleardb.com';
-        // private $dbname = 'heroku_9a37366182b5a0b';
-        // private $user = 'ba84dd51f09433';
-        // private $pass = '5de473f9';
-        private $host = 'localhost';
-         private $user = 'root';
-         private $pass = '';
-         private $dbname = 'rest_api';
+        private $host = 'eu-cdbr-west-01.cleardb.com';
+        private $dbname = 'heroku_9a37366182b5a0b';
+        private $user = 'ba84dd51f09433';
+        private $pass = '5de473f9';
+        // private $host = 'localhost';
+        //  private $user = 'root';
+        //  private $pass = '';
+        //  private $dbname = 'rest_api';
 
         public function connect() {
             $conn_str = "mysql:host=$this->host;dbname=$this->dbname";
