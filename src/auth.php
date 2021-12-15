ab9649757b201f371e6a58e14ea4f81f8fb7fb7c
SealSneezy <jakub.wypychowski@onet.pl>
2021-12-15 09:54:12 +0100

    #27 poprawki do zabezpieczeń

--- src/auth.php
+++ src/auth.php
@@ -4,56 +4,56 @@ use Psr\Http\Message\ServerRequestInterface;
 use Slim\Factory\AppFactory;
 use \Firebase\JWT\JWT;
 
-// $secret = "supersecret";
+$secret = "supersecret";
 
-// $app->add(new Tuupola\Middleware\JwtAuthentication([
-//     "secure" => false,
-//     "path" => ["/api/posts", "/api/categories"], 
-//     "algorithm" => ["HS256"],
-//     "secret" => $secret,
-//     "error" => function ($response, $arguments) {
-//         $data["status"] = "401";
-//         $data["message"] = $arguments["message"];
-//         return $response
-//             ->withHeader("Content-Type", "application/json")
-//             ->getBody()->write(json_encode($data));
-//         }
-// ]));
+$app->add(new Tuupola\Middleware\JwtAuthentication([
+    "secure" => false,
+    "path" => ["/api/posts", "/api/categories"], 
+    "algorithm" => ["HS256"],
+    "secret" => $secret,
+    "error" => function ($response, $arguments) {
+        $data["status"] = "401";
+        $data["message"] = $arguments["message"];
+        return $response
+            ->withHeader("Content-Type", "application/json")
+            ->getBody()->write(json_encode($data));
+        }
+]));
 
-// $app->post('/login', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
-//     global $secret; 
-//     $params = $request->getQueryParams();
-//     if(
-//         isset($params["email"], $params["password"])
-//     ){
-//         $email=$params["email"];
-//         $password=$params["password"];
-//         $query = "SELECT Has_access FROM Credentials WHERE Password=? AND Email=?";
-//         $matching_credentials = getFromDatabase($query, [$password, $email]);
-//         if(
-//             isset($matching_credentials[0])&&$matching_credentials[0]->Has_access=='1'
-//         ){
-//             $future = new DateTime("now +7 days");
-//             $jti = base64_encode(random_bytes(10));
-//             $payload = [
-//                 "jti" => $jti,
-//                 "exp" => $future->getTimeStamp()
-//             ];
-//             $token = JWT::encode($payload, $secret, "HS256");   
-//             $response->getBody()->write( json_encode(["status"=>"200","token"=>$token]));
-//             return $response
-//             ->withHeader('content-type', 'application/json')
-//             ->withStatus(200);
-//         }else{
-//             $response->getBody()->write( json_encode(["status"=>"404","message"=>'not found']));
-//             return $response
-//             ->withHeader('content-type', 'application/json')
-//             ->withStatus(404);
-//         }
-//     }else{
-//         $response->getBody()->write( json_encode(["status"=>"404","message"=>'provide credentials']));
-//             return $response
-//             ->withHeader('content-type', 'application/json')
-//             ->withStatus(200);
-//     }
-// });
+$app->post('/login', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
+    global $secret; 
+    $params = $request->getQueryParams();
+    if(
+        isset($params["email"], $params["password"])
+    ){
+        $email=$params["email"];
+        $password=$params["password"];
+        $query = "SELECT Has_access FROM Credentials WHERE Password=? AND Email=?";
+        $matching_credentials = getFromDatabase($query, [$password, $email]);
+        if(
+            isset($matching_credentials[0])&&$matching_credentials[0]->Has_access=='1'
+        ){
+            $future = new DateTime("now +7 days");
+            $jti = base64_encode(random_bytes(10));
+            $payload = [
+                "jti" => $jti,
+                "exp" => $future->getTimeStamp()
+            ];
+            $token = JWT::encode($payload, $secret, "HS256");   
+            $response->getBody()->write( json_encode(["status"=>"200","token"=>$token]));
+            return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200);
+        }else{
+            $response->getBody()->write( json_encode(["status"=>"404","message"=>'not found']));
+            return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(404);
+        }
+    }else{
+        $response->getBody()->write( json_encode(["status"=>"404","message"=>'provide credentials']));
+            return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200);
+    }
+});
